0124bcedf38a408c27c67752a1b8a7846a665ba5
Keita Shinyama <sk.shiyama127@icloud.com>
2021-01-14 00:07:15 +0900

    make repository

---
+++ agent.py
@@ -0,0 +1,37 @@
+import random as rnd
+
+from matplotlib.pyplot import colorbar
+import numpy as np
+
+class Agent:
+    def __init__(self):
+        self.point = 0.0
+        self.strategy = None
+        self.next_strategy = None
+        self.neighbors_id = []
+        self.strategy_map = []
+        self.color_map = []
+
+    def decide_next_strategy(self, agents):
+        # Pairwise-Fermiモデルで次のゲームでの戦略を決定する
+
+        opponent_id = rnd.choice(self.neighbors_id)
+        opponent = agents[opponent_id]
+
+        if opponent.strategy != self.strategy and rnd.random() < 1/(1 + np.exp((self.point - opponent.point)/0.1)):
+            self.next_strategy = opponent.strategy
+        else:
+            self.next_strategy = self.strategy 
+
+
+    def update_strategy(self):
+        self.strategy = self.next_strategy
+        return self.strategy
+        
+    def change_agents_color(self):
+        if self.strategy == 'C':
+            return "blue"
+        elif self.strategy == "D":
+            return "red"
+        
+        
---
+++ simulation.py
@@ -0,0 +1,106 @@
+import numpy as np
+import random as rnd
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+from agent import Agent
+
+class Simulation:
+
+    global strategy_map
+    global color_map
+    strategy_map = []
+    color_map = []
+
+    def __init__(self, population, average_degree):
+        self.agents = self.__generate_agents(population, average_degree)
+        self.initial_cooperators = self.__choose_initial_cooperators() 
+
+    def __generate_agents(self, population,edges):
+        # エージェントをリストに詰め、隣人エージェントのIDをセットする
+
+        global rearange_edges
+        global network
+        global pos
+        network = nx.barabasi_albert_graph(population, edges)
+        pos = nx.spring_layout(network)
+
+        agents = [Agent() for id in range(population)]
+        for index, focal in enumerate(agents):
+            neighbors_id = list(network[index])
+            for agent_id in neighbors_id:
+                focal.neighbors_id.append(agent_id)
+
+        return agents
+
+    def __choose_initial_cooperators(self):
+        #最初のゲームでC戦略を取るエージェントをランダムに選ぶ
+
+        population = len(self.agents)
+        initial_cooperators = rnd.sample(range(population), k = int(population/2))
+
+        return initial_cooperators
+
+    def __initialize_strategy(self):
+        # 全エージェントの戦略を初期化
+
+        for index, focal in enumerate(self.agents):
+            if index in self.initial_cooperators:
+                focal.strategy = "C"
+            else:
+                focal.strategy = "D"
+
+    def __count_payoff(self, Dg, Dr):
+        # 利得表に基づいて全エージェントが獲得する利得を計算
+
+        R = 1       # Reward
+        S = -Dr     # Sucker
+        T = 1+Dg    # Temptation
+        P = 0       # Punishment
+
+        for focal in self.agents:
+            focal.point = 0.0
+            for nb_id in focal.neighbors_id:
+                neighbor = self.agents[nb_id]
+                if focal.strategy == "C" and neighbor.strategy == "C":    
+                    focal.point += R 
+                elif focal.strategy == "C" and neighbor.strategy == "D":   
+                    focal.point += S
+                elif focal.strategy == "D" and neighbor.strategy == "C":   
+                    focal.point += T
+                elif focal.strategy == "D" and neighbor.strategy == "D":  
+                    focal.point += P
+
+    def __update_strategy(self):
+        # 全エージェントに戦略を更新させる
+
+        for focal in self.agents:
+            focal.decide_next_strategy(self.agents)
+
+        for focal in self.agents:
+            strategy_map.append(focal.update_strategy())
+    
+    def __change_agents_color(self):
+        # 戦略によりエージェントの色を変える
+
+        for focal in self.agents:
+            color_map.append(focal.change_agents_color())
+            
+
+    def __play_game(self, Dg, Dr):
+
+        self.__initialize_strategy()
+        self.__count_payoff(Dg, Dr)
+        self.__update_strategy()
+        self.__change_agents_color()
+
+    def plot_agents(self, Dg, Dr):
+        # エージェントの分布図の描画
+
+        self.__choose_initial_cooperators()
+        self.__play_game(Dg, Dr)
+        print(color_map)
+        plt.figure()
+        nx.draw_networkx(network, pos, node_color=color_map, with_labels=False, node_shape='.', node_size=100)
+        plt.axis("off")
+        plt.show()
