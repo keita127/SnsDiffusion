aaa885bd88dfd8edab1ff618efbb4d82806dc04a
Keita Shinyama <sk.shiyama127@icloud.com>
2021-01-15 02:26:57 +0900

    初期状態を描画できるように変更

--- simulation.py
+++ simulation.py
@@ -106,6 +106,7 @@ class Simulation:
 
     def plot_agents(self, Dg, Dr):
         # エージェントの分布図の描画
+        # gitまじでわからん、、
         self.__choose_initial_cooperators()
         self.__initialize_strategy()
         self.__change_agents_color()
@@ -114,3 +115,4 @@ class Simulation:
         plt.axis("off")
         plt.show()
         self.__play_game(Dg, Dr)
+
