0fcd5c3036cec1a886320d5ce38d3a93293245c4
Keita Shinyama <sk.shiyama127@icloud.com>
2021-01-22 05:26:12 +0900

    [update]保存階層の変更

--- animation.py
+++ animation.py
@@ -6,24 +6,30 @@ import matplotlib.animation as animation
 
 
 #該当フォルダから画像のリストを取得。読み込みたいファイル形式を指定。ここではpng 
-picList = glob.glob("*.png")
-picList.sort()
 
-fig = plt.figure(figsize=(20,20),dpi=100)
+def make_gif():
+    for i in range(11):
+        I = i * 0.1
+        I = round(I, 1)
+        
+        picList = glob.glob("images/images_" + str(I) + "/*.png")
+        picList.sort()
+        fig = plt.figure(figsize=(20,20),dpi=100)
 
-#空のリスト作成
-ims = []
-     
-#画像ファイルを空のリストの中に1枚ずつ読み込み
-for i in range(len(picList)):
-         
-    #読み込んで付け加えていく
-    tmp = Image.open(picList[i])
-    ims.append([plt.imshow(tmp)])      
+        #空のリスト作成
+        ims = []
 
-#アニメーション作成  
-plt.axis('off')
-ani = animation.ArtistAnimation(fig, ims, interval=1000)
+        #画像ファイルを空のリストの中に1枚ずつ読み込み
+        for i in range(len(picList)):
+                
+            #読み込んで付け加えていく
+            tmp = Image.open(picList[i])
+            ims.append([plt.imshow(tmp)])      
 
-#アニメーション保存。ファイル名を入れてください。ここではtest.gif
-ani.save("test.gif", writer='pillow')
+        #アニメーション作成  
+        plt.axis('off')
+        ani = animation.ArtistAnimation(fig, ims, interval=1000)
+
+        #アニメーション保存。ファイル名を入れてください。ここではtest.gif
+        ani.save("images/images_" + str(I) + "/animation_" + str(I) + ".gif", writer='pillow')
+        print(str(I) + "is OK")
