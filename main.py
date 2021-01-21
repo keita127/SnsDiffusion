8f01445dd7850f07a3a6d4831444ac42b1e7750b
Keita Shinyama <sk.shiyama127@icloud.com>
2021-01-22 05:24:17 +0900

    [update]animation.pyの呼び出し

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from simulation import Simulation
+import animation
 
 def main():
-    population = 300          # エージェント数
+    population = 500          # エージェント数
     edges = 5        # 社会ネットワークの平均次数
-    Dg = 0.8
-    Dr = 0.2
     simulation = Simulation(population, edges)
     
-    simulation.plot_agents(Dg, Dr)
+    for i in range(11):
+        I = i * 0.1
+        I = round(I, 1)
+        print("I=" + str(I))
+        simulation.plot_agents(I, population)
+
+    animation.make_gif()
 
 if __name__ == '__main__':
     main()
